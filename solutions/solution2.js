b0a20c3300781f7daabeae813261d5891fe08821
Maryus Martsyalis <martsyalis@gmail.com>
2019-09-20 11:40:13 -0700

    second Solution

---
+++ solutions/solution2.js
@@ -0,0 +1,8 @@
+const iceCreamUl = document.querySelector(".ice-cream-list");
+
+for (flavor of window.iceCreamFlavors) {
+  const iceCreamLi = document.createElement("li");
+  iceCreamLi.classList.add("ice-cream-flavor");
+  iceCreamLi.innerText = flavor;
+  iceCreamUl.appendChild(iceCreamLi);
+}
