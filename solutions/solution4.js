95cb78d6e7ef4491a46c22af2b3393188b878b84
Maryus Martsyalis <martsyalis@gmail.com>
2019-09-20 11:48:38 -0700

    fourth solution

---
+++ solutions/solution4.js
@@ -0,0 +1,9 @@
+//very similar to 3rd
+
+const iceCreamUl = document.querySelector(".ice-cream-list");
+
+const flavorElementsString = window.iceCreamFlavors.reduce((acc, flavor) => {
+  return acc + `<li class="ice-cream-flavor">${flavor}</li>`;
+}, "");
+
+iceCreamUl.innerHTML = flavorElementsString;
