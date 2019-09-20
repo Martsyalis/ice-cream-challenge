615217cfbcf0158210e32f9e63fffb3e97cd0e12
Maryus Martsyalis <martsyalis@gmail.com>
2019-09-20 11:56:07 -0700

    adds spacing to solution1

--- solutions/solution1.js
+++ solutions/solution1.js
@@ -1,5 +1,6 @@
 (function() {
   const $iceCreamList = $(".ice-cream-list");
+  
   window.iceCreamFlavors.forEach(flavor => {
     const $iceCreamItem = $("<li>")
       .attr("class", "ice-cream-flavor")
